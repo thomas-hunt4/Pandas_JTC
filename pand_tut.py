ddeadc29eb501036dec64f7a569a29da00ea5737
Thomas Hunt <thomas.a.hunt.4@gmail.com>
2025-09-21 21:31:36 -0300

    venvaches

---
+++ pand_tut.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
+
+print(df)
+
+max_pull = df["Age"].max()
+print(max_pull)
+
+ages = pd.Series([22, 35, 58], name="Age")
+
+print(ages.max())
+
+df.describe()
+
+
+
+
+
